5f45032eb91a17bde2d83538e0878fb7065cffb7
rray524 <rray524@gmail.com>
2022-01-08 16:30:17 +0600

    react & dom installed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-
-
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import Home from "./pages/Home";
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/registration">
+          <Register />
+        </Route>
+      </Switch>
+    </Router >
   );
 }
 
