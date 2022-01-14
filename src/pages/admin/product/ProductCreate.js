b5acc587a792b6209939733f2ec216fdbce4e637
rray524 <rray524@gmail.com>
2022-01-14 23:31:08 +0600

    multiple image options created

--- src/pages/admin/product/ProductCreate.js
+++ src/pages/admin/product/ProductCreate.js
@@ -5,6 +5,8 @@ import { createProduct } from "../../../functions/product";
 import ProductCreateForm from "../../../components/forms/ProductCreateForm";
 import { getCategories, getCategorySubs } from "../../../functions/category";
 import AdminNav from "../../../components/AdminNav";
+import FileUpload from "../../../components/forms/FileUpload";
+import { LoadingOutlined } from "@ant-design/icons";
 
 const initialState = {
     title: "Macbook Pro",
@@ -26,6 +28,7 @@ const ProductCreate = () => {
     const [values, setValues] = useState(initialState);
     const [subOptions, setSubOptions] = useState([]);
     const [showSub, setShowSub] = useState(false);
+    const [loading, setLoading] = useState(false);
 
     // redux
     const user = useSelector(state => state.user.loggedInUser);
@@ -77,10 +80,22 @@ const ProductCreate = () => {
                 </div>
 
                 <div className="col-md-10">
-                    <h4>Product create</h4>
+                    {loading ? (
+                        <LoadingOutlined className="text-danger h1" />
+                    ) : (
+                        <h4>Product create</h4>
+                    )}
                     <hr />
 
-                    {JSON.stringify(values.subs)}
+                    {/* {JSON.stringify(values.images)} */}
+
+                    <div className="py-3">
+                        <FileUpload
+                            values={values}
+                            setValues={setValues}
+                            setLoading={setLoading}
+                        />
+                    </div>
 
                     <ProductCreateForm
                         handleSubmit={handleSubmit}
