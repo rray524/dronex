14ce1ef6e8100dff7708402f690858701a3f7eac
rray524 <rray524@gmail.com>
2022-01-20 16:30:56 +0600

    fixed navigation issue

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -69,8 +69,8 @@ const Navigation = () => {
             {(user && user.role === "admin") && <Item key="admin-dashboard" icon={<MenuOutlined />} style={{ display: 'flex', alignItems: 'center', fontSize: "18px" }}>
                 <Link to="/admin/dashboard">Admin Dashboard</Link>
             </Item>}
-            {!user && <Item key="login" icon={<UserOutlined />} style={{ display: 'flex', alignItems: 'center', fontSize: "18px" }}><Link to="/login">Login</Link></Item>}
-            {!user && <Item key="register" icon={<UserAddOutlined />} style={{ display: 'flex', alignItems: 'center', fontSize: "18px" }}><Link to="/registration">Register</Link></Item>
+            {!user?.email && <Item key="login" icon={<UserOutlined />} style={{ display: 'flex', alignItems: 'center', fontSize: "18px" }}><Link to="/login">Login</Link></Item>}
+            {!user?.email && <Item key="register" icon={<UserAddOutlined />} style={{ display: 'flex', alignItems: 'center', fontSize: "18px" }}><Link to="/registration">Register</Link></Item>
             }
             {user?.email && <Item key="logout" icon={<LogoutOutlined />} onClick={logout} style={{ display: 'flex', alignItems: 'center', fontSize: "18px" }}><Link to="/logout">Logout</Link></Item>}
             {user && <div style={{ display: 'flex', alignItems: 'center', fontSize: "18px" }}>{< UserAddOutlined />} <p style={{ display: 'flex', alignItems: 'center', fontSize: "18px", margin: "0 6px 0 8px", textTransform: 'capitalize' }}> {user?.email?.split("@")[0]}</p></div>}
